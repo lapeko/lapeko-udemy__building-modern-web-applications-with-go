9ddb4c841ee6c18f8e8a5f16131cf25673d979c6
lapeko <lapeko88@gmail.com>
2024-08-13 07:43:41 +0200

    refactoring. add about route

--- chapter2/6-testing/main.go
+++ chapter2/6-testing/main.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 )
 
-func Divide(dividend float32, divider float32) (float32, error) {
+func Divide(dividend, divider float32) (float32, error) {
 	if divider == 0 {
 		return 0.0, errors.New("divider can not be null")
 	}
--- chapter3/main.go
+++ chapter3/main.go
@@ -5,13 +5,25 @@ import (
 	"net/http"
 )
 
+const PORT = ":3011"
+
+func aboutHandler(w http.ResponseWriter, req *http.Request) {
+	a, b := 1, 2
+	sumRes := sum(a, b)
+	_, _ = fmt.Fprintf(w, "Sum of %d and %d is %d", a, b, sumRes)
+}
+
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	_, _ = fmt.Fprintf(w, "Home page")
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		n, err := fmt.Fprint(w, "Hello world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Success. %d bites sent\n", n)
-	})
-	_ = http.ListenAndServe(":3011", nil)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	fmt.Println("Server is running on port", PORT)
+	_ = http.ListenAndServe(PORT, nil)
 }
