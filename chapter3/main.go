0024ffd86cec6e0d6ba6e3a4583779811c41f66d
lapeko <lapeko88@gmail.com>
2024-08-13 08:37:00 +0200

    using html teamplates

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,48 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const PORT = ":3011"
 
-func aboutHandler(w http.ResponseWriter, req *http.Request) {
-	a, b := 1, 2
-	sumRes := sum(a, b)
-	_, _ = fmt.Fprintf(w, "Sum of %d and %d is %d", a, b, sumRes)
-}
-
 func homeHandler(w http.ResponseWriter, req *http.Request) {
-	_, _ = fmt.Fprintf(w, "Home page")
+	renderPage(w, "home")
 }
 
-func divideHandler(w http.ResponseWriter, req *http.Request) {
-	a, b := 10.0, 5.0
-	res, err := divide(a, b)
-	if err != nil {
-		_, _ = fmt.Fprintf(w, "Can not devide %.2f by %.2f", a, b)
-		return
-	}
-	_, _ = fmt.Fprintf(w, "Divide %.2f by %.2f is %.2f", a, b, res)
-}
-
-func sum(a, b int) int {
-	return a + b
-}
-
-func divide(dividend, divider float64) (float64, error) {
-	if divider == 0 {
-		return 0, errors.New("divide by 0")
-	}
-	return dividend / divider, nil
+func aboutHandler(w http.ResponseWriter, req *http.Request) {
+	renderPage(w, "about")
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/divide", divideHandler)
 	fmt.Println("Server is running on port", PORT)
 	_ = http.ListenAndServe(PORT, nil)
+}
+
+func renderPage(w http.ResponseWriter, pageName string) {
+	templatePath := "./templates/" + pageName + ".html"
+	_template, err := template.ParseFiles(templatePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = _template.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
