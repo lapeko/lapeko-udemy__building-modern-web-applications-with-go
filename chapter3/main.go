84a0874a4a642f8eb91c57b8932302145b93888e
lapeko <lapeko88@gmail.com>
2024-08-13 08:03:01 +0200

    add error handling to http handler

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -17,13 +18,31 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 	_, _ = fmt.Fprintf(w, "Home page")
 }
 
+func divideHandler(w http.ResponseWriter, req *http.Request) {
+	a, b := 10.0, 5.0
+	res, err := divide(a, b)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "Can not devide %.2f by %.2f", a, b)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "Divide %.2f by %.2f is %.2f", a, b, res)
+}
+
 func sum(a, b int) int {
 	return a + b
+}
+
+func divide(dividend, divider float64) (float64, error) {
+	if divider == 0 {
+		return 0, errors.New("divide by 0")
+	}
+	return dividend / divider, nil
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/divide", divideHandler)
 	fmt.Println("Server is running on port", PORT)
 	_ = http.ListenAndServe(PORT, nil)
 }
